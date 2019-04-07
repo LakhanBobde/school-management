46ac05a8123c4c88dcf5cb18e48883ed2673508e
LakhanBobde <45265451+LakhanBobde@users.noreply.github.com>
2019-04-08 00:13:40 +0530

    Add files via upload

---
+++ admin/inc/js.php
@@ -0,0 +1,17 @@
+<!-- plugins:js -->
+<script src="https://omo24x7.github.io/cdn/s5admin/jquery/dist/jquery.min.js"></script>
+  <script src="https://omo24x7.github.io/cdn/s5admin/popper.js/dist/umd/popper.min.js"></script>
+  <script src="https://omo24x7.github.io/cdn/s5admin/bootstrap/dist/js/bootstrap.min.js"></script>
+  <!-- endinject -->
+  <!-- Plugin js for this page-->
+  <script src="https://omo24x7.github.io/cdn/s5admin/flot/jquery.flot.js"></script>
+  <script src="https://omo24x7.github.io/cdn/s5admin/flot/jquery.flot.resize.js"></script>
+  <script src="https://omo24x7.github.io/cdn/s5admin/flot.curvedlines/curvedLines.js"></script>
+  <!-- End plugin js for this page-->
+  <!-- inject:js -->
+  <script src="https://omo24x7.github.io/cdn/s5admin/js/off-canvas.js"></script>
+  <script src="https://omo24x7.github.io/cdn/s5admin/js/misc.js"></script>
+  <!-- endinject -->
+  <!-- Custom js for this page-->
+  <script src="https://omo24x7.github.io/cdn/s5admin/js/dashboard.js"></script>
+  <!-- End custom js for this page-->
