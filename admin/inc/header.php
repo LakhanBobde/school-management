c246957d60f4910a619d9a411cf8066543c2b97e
LakhanBobde <45265451+LakhanBobde@users.noreply.github.com>
2019-04-08 00:12:41 +0530

    Add files via upload

---
+++ admin/inc/header.php
@@ -0,0 +1,20 @@
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title>Stellar Admin</title>
+  <!-- plugins:css -->
+  <link rel="stylesheet" href="https://omo24x7.github.io/cdn/s5admin/mdi/css/materialdesignicons.min.css">
+  <link rel="stylesheet" href="https://omo24x7.github.io/cdn/s5admin/simple-line-icons/css/simple-line-icons.css">
+  <link rel="stylesheet" href="https://omo24x7.github.io/cdn/s5admin/flag-icon-css/css/flag-icon.min.css">
+  <!-- endinject -->
+  <!-- plugin css for this page -->
+  <link rel="stylesheet" href="https://omo24x7.github.io/cdn/s5admin/chartist/dist/chartist.min.css" />
+  <link rel="stylesheet" href="https://omo24x7.github.io/cdn/s5admin/jvectormap/jquery-jvectormap.css" />
+  <!-- End plugin css for this page -->
+  <!-- inject:css -->
+  <link rel="stylesheet" href="https://omo24x7.github.io/cdn/s5admin/css/style.css">
+  <!-- endinject -->
+  <link rel="shortcut icon" href="https://omo24x7.github.io/cdn/s5admin/images/favicon.png" />
+</head>
